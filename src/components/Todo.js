3eae46a08161879acd2df637d06e7bedff71b7e8
shahinabbas <shahinabbas771@gmail.com>
2023-09-18 22:26:59 +0530

    add,complete,delte

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -35,34 +35,38 @@ function Todo() {
         })
         setTodos(complete)
     }
-    const onEdit = (id => {
-        setTodos([...todos,])
-    })
+    // const onEdit = (id => {
+    //     setTodos([...todos,])
+    // })
 
     return (
-        <div className='container'>
-            <h2>Todo</h2>
-            <form className='form-group' onSubmit={handleSubmit}>
-                <input type='text' value={todo} ref={inputRef} placeholder='Enter task....' className='form-control' onChange={(event) => setTodo(event.target.value)} />
-                <button onClick={addTodo}>Add</button>
-            </form>
-            <div>
-                <ul className='list'>
-                    {
-                        todos.map((to) => (
-                            <li className='list-items'>
-                                <div className='list-item-list' id={to.status ? 'list-item' : ''}>{to.list}</div>
-                                <span>
-                                    <TiTick className='list-item-icons' id='complete' title='completr' onClick={() => onComplete(to.id)} />
-                                    <AiFillEdit className='list-item-icons' id='edit' title='edit' onClick={() => ondevicemotion(to.id)} />
-                                    <AiFillDelete className='list-item-icons' id='delete' title='delete' onClick={() => onDelete(to.id)} />
-                                </span>
-                            </li>
-                        ))
-                    }
-                </ul>
+        <div className='head'><h2>Organize  your  work  and  life.</h2>
+            <div className='container'>
+                <h2>Todo</h2>
+                <form className='form-group' onSubmit={handleSubmit}>
+                    <input type='text' value={todo} ref={inputRef} placeholder='Enter task....' className='form-control' onChange={(event) => setTodo(event.target.value)} />
+                    <button onClick={addTodo}>Add</button>
+                </form>
+                <div>
+                    <ul className='list'>
+                        {
+                            todos.map((to) => (
+                                <li className='list-items'>
+                                    <div className='list-item-list' id={to.status ? 'list-item' : ''}>{to.list}</div>
+                                    <span>
+                                        <TiTick className='list-item-icons' id='complete' title='completr' onClick={() => onComplete(to.id)} />
+                                        <AiFillEdit className='list-item-icons' id='edit' title='edit' onClick={() => ondevicemotion(to.id)} />
+                                        <AiFillDelete className='list-item-icons' id='delete' title='delete' onClick={() => onDelete(to.id)} />
+                                    </span>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                
             </div>
         </div>
+        
     )
 }
 export default Todo
